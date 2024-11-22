536ed1e66bbd99157d4852f3091f7e3e02e7f241
Piotr Matlak <piotr.matlak.contact@gmail.com>
2024-11-22 15:20:27 +0100

    config prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { HeaderSection } from '@/sections/HeaderSection'
 export default function Home() {
   return (
     <div className='min-h-screen min-w-screen'>
-      <Navbar />
       <HeaderSection />
     </div>
   )
