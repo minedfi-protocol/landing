ec89fc164c319a036a292430decf3b59b1724846
kuba80-02 <kuba8369@gmail.com>
2024-11-23 09:54:04 +0100

    feat: implement AboutSection with video playback and responsive design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Navbar from '@/components/Navbar/Navbar'
+import { About } from '@/sections/AboutSection'
 import { HeaderSection } from '@/sections/HeaderSection'
 export default function Home() {
   return (
     <div className='min-h-screen min-w-screen'>
       <Navbar />
       <HeaderSection />
+      <About />
     </div>
   )
 }
