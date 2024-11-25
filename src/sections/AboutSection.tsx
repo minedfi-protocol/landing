07889b9034fddfc52a9e1647401b522a8f8b0672
kuba80-02 <kuba8369@gmail.com>
2024-11-25 20:12:27 +0100

    Remove redundant labels and adjust heading margins in AboutSection and TokenSection for cleaner presentation

--- src/sections/AboutSection.tsx
+++ src/sections/AboutSection.tsx
@@ -45,8 +45,7 @@ export const About = () => {
   return (
     <section className='relative flex flex-col items-center px-5 pb-5 gap-16 w-full bg-[#FFFCF9] overflow-hidden'>
       <div>
-        <p className='text-desktop-label text-center text-light-gray'>About</p>
-        <h2 className='text-mobile-h2 lg:text-desktop-h2 text-neutral-60 text-center lg:w-[800px] mt-[12px]'>
+        <h2 className='text-mobile-h2 lg:text-desktop-h2 text-neutral-60 text-center lg:w-[800px] mt-7'>
           Redefining mining through liquid staking
         </h2>
         <p className='text-neutral-40 text-mobile-subtitle lg:text-desktop-subtitle lg:w-[800px] text-center mt-2 lg:mt-20'>
--- src/sections/TokenSection.tsx
+++ src/sections/TokenSection.tsx
@@ -6,9 +6,6 @@ export const TokenSection = () => {
   return (
     <section className='relative flex flex-col items-center px-5 pb-5 gap-16 w-full bg-[#FFFCF9] overflow-hidden'>
       <div>
-        <p className='text-mobile-label md:text-desktop-label  text-center text-light-gray'>
-          Tokens
-        </p>
         <h2 className='text-mobile-h2 lg:text-desktop-h2 text-neutral-60 text-center lg:w-[800px] mt-[12px]'>
           Stake native tokens, receive LMTs and mining rewards
         </h2>
