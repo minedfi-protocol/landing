1b01d379233474b1ea9e807643f3c1152f7cce53
kuba80-02 <kuba8369@gmail.com>
2024-11-26 16:03:21 +0100

    add Energy and TVL SVG images; update HeaderSection to display new metrics

--- src/sections/HeaderSection.tsx
+++ src/sections/HeaderSection.tsx
@@ -36,10 +36,10 @@ export const HeaderSection = () => {
         </div>
       </div>
       <div className='relative z-10 w-full'>
-        <div className='absolute -left-0 top-5 h-[100px] w-[200px] bg-apy_mobile bg-cover bg-center md:-top-5 md:h-[146px] md:w-[298px] md:bg-apy' />
+        {/* <div className='absolute -left-0 top-5 h-[100px] w-[200px] bg-apy_mobile bg-cover bg-center md:-top-5 md:h-[146px] md:w-[298px] md:bg-apy' />
         <div className='absolute -right-0 top-5 h-[100px] w-[200px] bg-tvl_mobile bg-cover bg-center md:-top-5 md:h-[146px] md:w-[298px] md:bg-tvl' />
         <div className='absolute -left-0 bottom-8 h-[100px] w-[200px] bg-hash_mobile bg-cover bg-center md:h-[146px] md:w-[340px] md:bg-hashrate' />
-        <div className='absolute -right-0 bottom-8 h-[100px] w-[200px] bg-energy_mobile bg-cover bg-center md:h-[146px] md:w-[340px] md:bg-energy' />
+        <div className='absolute -right-0 bottom-8 h-[100px] w-[200px] bg-energy_mobile bg-cover bg-center md:h-[146px] md:w-[340px] md:bg-energy' /> */}
 
         <div className='flex min-h-[686px] w-full flex-col items-center justify-center bg-transparent'>
           <h1 className='bg-gradient-to-r from-[#180F08] to-[#5D544D] bg-clip-text p-4 text-center text-mobile-h1 text-transparent md:text-desktop-h1'>
@@ -58,6 +58,52 @@ export const HeaderSection = () => {
               className='ml-[8px]'
             />
           </button>
+        </div>
+        <div className='md:space mb-16 flex flex-row-reverse items-center justify-center px-7 md:flex-row md:space-x-4 md:px-0'>
+          <div className='flex flex-col items-center justify-center md:flex-row'>
+            <Image
+              src='/assets/images/hero-images-labels/APY.svg'
+              alt='Energy'
+              width={56}
+              height={56}
+            />
+            <span className='ml-2 text-center text-desktop-title-s text-neutral-80'>
+              APY <span className='font-bold'>5.5%</span>
+            </span>
+          </div>
+          <div className='flex flex-col items-center justify-center md:flex-row'>
+            <Image
+              src='/assets/images/hero-images-labels/Hashrate.svg'
+              alt='Energy'
+              width={56}
+              height={56}
+            />
+            <span className='ml-2 text-center text-desktop-title-s text-neutral-80'>
+              Hashrate <span className='font-bold'>120TH/s</span>
+            </span>
+          </div>
+          <div className='flex flex-col items-center justify-center md:flex-row'>
+            <Image
+              src='/assets/images/hero-images-labels/TVL.svg'
+              alt='TVL'
+              width={56}
+              height={56}
+            />
+            <span className='ml-2 text-center text-desktop-title-s text-neutral-80'>
+              TVL <span className='font-bold'>$500M</span>
+            </span>
+          </div>
+          <div className='hidden flex-col items-center justify-center md:flex md:flex-row'>
+            <Image
+              src='/assets/images/hero-images-labels/Energy.svg'
+              alt='Energy'
+              width={56}
+              height={56}
+            />
+            <span className='ml-2 text-desktop-title-s text-neutral-80'>
+              Green Energy <span className='font-bold'>80%</span>
+            </span>
+          </div>
         </div>
       </div>
     </section>
